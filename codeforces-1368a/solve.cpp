9e85a6c679f9077508055d10093126ef28ce29ca
Azarel Grahandito <skootydack@gmail.com>
2024-01-27 01:24:35 +0700

    25-jan-24

--- codeforces-1368a/solve.cpp
+++ codeforces-1368a/solve.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
+typedef unsigned long long ull;
 
 int main(int argc, char *argv[]) {
   ios_base::sync_with_stdio(false);
@@ -8,7 +9,19 @@ int main(int argc, char *argv[]) {
   int T;
   cin >> T;
   while (T--) {
-    int a, b, n;
+    ull a, b, n;
     cin >> a >> b >> n;
+    ull ans = 0;
+    int operations = 0;
+    while (ans <= n) {
+      if (a > b) {
+        b += a;
+      } else {
+        a += b;
+      }
+      ans = max(a, b);
+      operations++;
+    }
+    cout << operations << '\n';
   }
 }
