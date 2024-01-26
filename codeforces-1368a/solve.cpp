f42f4f1ac486bcdd4f86fa5289c07272ecf7647e
Azarel Grahandito <skootydack@gmail.com>
2024-01-26 14:14:30 +0700

    attempting for 25-jan-24

---
+++ codeforces-1368a/solve.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    int a, b, n;
+    cin >> a >> b >> n;
+  }
+}
