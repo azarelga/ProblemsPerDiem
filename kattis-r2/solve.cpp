ae5e916c377546c9c4e31e6700504d3286411ee4
Azarel Grahandito <skootydack@gmail.com>
2024-01-29 14:30:43 +0700

    29-jan-24

---
+++ kattis-r2/solve.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int a, b;
+  cin >> a >> b;
+  cout << 2 * b - a << endl;
+}
